59f873ca09776e5dcaa2a41c0eec91b79d96ae9a
alefrodrigues <alefrodrigues@gmail.com>
2018-05-29 22:54:45 -0300

    Rev-003
    Adicionado sistema de deletar e criar blocos usando clicks do mouse.
    Fixado metodo de captaçao da posiçao do mouse em relaçao ao mundo

--- Terraria_Remake-UnityProject/Assets/Scripts/proceduralGrid.cs
+++ Terraria_Remake-UnityProject/Assets/Scripts/proceduralGrid.cs
@@ -20,17 +20,60 @@ public class proceduralGrid : MonoBehaviour {
 
 	public Transform player;
 
+	public int idBlocoDeletar;
+	public int idBlocoCriar;
+
+	public Camera myCam;
+
+	public GameObject obj1;
+	public GameObject obj2;
+	RaycastHit hit; 
 	void Awake(){
 		mesh = GetComponent<MeshFilter>().mesh;
-		GetComponent<MeshCollider>().sharedMesh = mesh;
+
 	}
 	void Start () {
 
 	}
-	void Update(){
+	void FixedUpdate(){
 		if(Input.GetKeyDown(KeyCode.Space)){
 			makeDiscreteGrid();
 			updateMesh();
+		}
+		if(Input.GetKeyDown(KeyCode.X)){
+			deletarBloco (idBlocoDeletar);
+		}
+
+		if(Input.GetKeyDown(KeyCode.C)){
+			criarBloco(idBlocoDeletar);
+		}
+
+		if (Input.GetKey(KeyCode.Mouse0)) {
+			Vector2 p = myCam.ScreenToWorldPoint (new Vector3(Input.mousePosition.x,Input.mousePosition.y));
+
+			Vector2 posMouse = p;
+	
+			int posX =  Mathf.FloorToInt(Mathf.Round(posMouse.x - 0.5f));
+			int posY = Mathf.FloorToInt(Mathf.Round(posMouse.y - 0.5f));
+			print (posX+" , "+posY);
+
+			int valorTriangulo = (posY + (posX * 100));
+
+			deletarBloco (valorTriangulo);
+		}
+
+		if (Input.GetKey(KeyCode.Mouse1)) {
+			Vector2 p = myCam.ScreenToWorldPoint (new Vector3(Input.mousePosition.x,Input.mousePosition.y));
+
+			Vector2 posMouse = p;
+
+			int posX =  Mathf.FloorToInt(Mathf.Round(posMouse.x - 0.5f));
+			int posY = Mathf.FloorToInt(Mathf.Round(posMouse.y - 0.5f));
+			print (posX+" , "+posY);
+
+			int valorTriangulo = (posY + (posX * 100));
+
+			criarBloco (valorTriangulo);
 		}
 	}
 	
@@ -51,17 +94,23 @@ public class proceduralGrid : MonoBehaviour {
 			for(int y = 0; y < gridSizeY; y++){
 				Vector3 cellOffSet = new Vector3(x * cellSize,y * cellSize, 0);
 
-				vertices[v] = new Vector3( -VertexOffSet, -VertexOffSet, 0 ) + cellOffSet + gridOffSet;
+				vertices [v] = new Vector3 (x,y,0);
+				vertices [v+1] = new Vector3 (x,y+1,0);
+				vertices [v+2] = new Vector3 (x+1,y,0);
+				vertices [v+3] = new Vector3 (x+1,y+1,0);
+
+				/*vertices[v] = new Vector3( -VertexOffSet, -VertexOffSet, 0 ) + cellOffSet + gridOffSet;
 				vertices[v+1] = new Vector3( -VertexOffSet, VertexOffSet, 0 ) + cellOffSet  + gridOffSet;
 				vertices[v+2] = new Vector3( VertexOffSet, -VertexOffSet, 0 ) + cellOffSet  + gridOffSet;
-				vertices[v+3] = new Vector3( VertexOffSet, VertexOffSet, 0 ) + cellOffSet  + gridOffSet;
+				vertices[v+3] = new Vector3( VertexOffSet, VertexOffSet, 0 ) + cellOffSet  + gridOffSet;*/
 
 				triangles[t] = v;
 				triangles[t+1] = triangles[t+4] = v+ 1;
 				triangles[t+2] = triangles[t+3] = v+ 2;
 				triangles[t+5] = v + 3;
-
 				setUvBlock(4);
+
+				//print (vertices[v] + " ," +vertices[v+1] + " ," +vertices[v+2] + " ," +vertices[v+3]);
 
 				v += 4;
 				t += 6;
@@ -70,12 +119,41 @@ public class proceduralGrid : MonoBehaviour {
 		}
 	}
 
+	void deletarBloco(int idBloco){
+		idBloco = idBloco * 4;
+		for(int i=0;i<=triangles.Length;i++){
+			if(triangles[i] == idBloco && idBloco <= triangles.Length){
+				triangles [i] = idBloco;
+				triangles [i + 1] = triangles [i + 4] = idBloco;
+				triangles [i + 2] = triangles [i + 3] = idBloco;
+				triangles [i + 5] = idBloco;
+				break;
+			}
+		}
+		updateMesh ();
+	}
+
+	void criarBloco(int idBloco){
+		idBloco = idBloco * 4;
+		for(int i=0;i<=triangles.Length;i++){
+			if(triangles[i] == idBloco && idBloco <= triangles.Length){
+				triangles [i] = idBloco;
+				triangles [i + 1] = triangles [i + 4] = idBloco+1;
+				triangles [i + 2] = triangles [i + 3] = idBloco+2;
+				triangles [i + 5] = idBloco+3;
+				break;
+			}
+		}
+		updateMesh ();
+	}
+
 	void updateMesh(){
 		mesh.Clear();
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.uv = uvList.ToArray();
 		mesh.RecalculateNormals();
+		GetComponent<MeshCollider>().sharedMesh = mesh;
 	}
 
 	void setUvBlock (int _idTypeBloco) {
